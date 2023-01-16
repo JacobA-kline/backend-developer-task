510c359470cceb502ae79b236ee1bb71328f9788
Jacob Kline <kobekline@gmail.com>
2023-01-16 13:58:25 +0200

    fix push

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -7,9 +7,10 @@
 # Description:
 # Push image to dockerhub
 
- echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    for container in $(docker-compose config --services); do
-        echo "Pushing $container"
-        docker tag "$container" "$DOCKER_USERNAME"/usermanagement/"$container"
-        docker push "$DOCKER_USERNAME"/usermanagement/"$container"
-    done
+docker images
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  for container in $(docker-compose config --services); do
+      echo "Pushing $container"
+      docker tag "$container" "$DOCKER_USERNAME"/usermanagement/"$container"
+      docker push "$DOCKER_USERNAME"/usermanagement/"$container"
+  done
