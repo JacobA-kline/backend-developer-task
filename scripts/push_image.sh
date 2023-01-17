e7e69d32e9f2ab1a35d645fdb45417d8dba5aaf5
Jacob Kline <kobekline@gmail.com>
2023-01-17 08:55:28 +0200

    fix for push script

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -15,7 +15,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
   for service in $(docker-compose config --services); do
     echo "Tagging and pushing $service:$version"
-    docker tag "$service":latest "$DOCKER_USERNAME"/usermanagement/"$service":"$version"
-#    docker push "$DOCKER_USERNAME"/usermanagement/"$service":"$version"
+    docker tag "$service":latest "$DOCKER_USERNAME"/usermanagement/:"$service""$version"
+    docker push "$DOCKER_USERNAME"/usermanagement/:"$service""$version"
   done
 docker images
