094a24ea3b80da408ea58df2dfce709a2dde8260
Jacob Kline <kobekline@gmail.com>
2023-01-18 09:10:32 +0200

    add another endpoint to test in test_app.sh script

--- scripts/test_app.sh
+++ scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 # Author: Jacob Kline
 # Date: 16/01/23
-# Last Modified: 16/01/23
+# Last Modified: 18/01/23
 
 # Description:
 # Test for app endpoint @ http://localhost/user-management/
@@ -12,5 +12,12 @@ curl -I http://localhost/user-management/
 status_code=$?
 if [ $status_code -ne 0 ]; then
   echo "Error: Unexpected status code $status_code"
+  exit 1
+fi
+
+curl -I http://localhost:5000/users/all
+status_code=$?
+if [ $status_code -ne 0 ]; then
+  echo "Error: Unexpected status code $status_code for /users/all"
   exit 1
 fi
